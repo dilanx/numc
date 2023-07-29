1f2fcb60b517a3043486bb8b8a46c68f034d0ef2
Dilan <dilan@dilanxd.com>
2023-07-28 20:06:06 -0700

    remove unused imports

--- src/main/kotlin/com/dilanxd/mcp/numc/NUMC.kt
+++ src/main/kotlin/com/dilanxd/mcp/numc/NUMC.kt
@@ -1,12 +1,9 @@
 package com.dilanxd.mcp.numc
 
-import org.bukkit.plugin.java.JavaPlugin
-import org.bukkit.command.CommandSender
-import com.dilanxd.mcp.numc.WhitelistCheck
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
-import org.bukkit.command.Command
-import org.bukkit.entity.Player
 import com.dilanxd.mcp.numc.Utility.Companion.sendMessage
+import org.bukkit.command.Command
+import org.bukkit.command.CommandSender
+import org.bukkit.plugin.java.JavaPlugin
 
 class NUMC : JavaPlugin() {
     override fun onEnable() {
